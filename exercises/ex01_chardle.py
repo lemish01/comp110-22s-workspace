fc77485e1de8012bc23bc815e1b44435d4502d15
Michelle <lemish@ad.unc.edu>
2022-01-24 18:20:42 -0500

    worked on ex01

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -3,7 +3,42 @@
 _author_ = "730407722"
  
 desired_word: str = input("Enter a 5-character word:") 
+
+if len(desired_word) != 5:
+    print("Error: Word must contain 5 characters")
+    exit() 
  
 entered_character: str = input("Enter a single character:")
 
 print("Searching for "+ entered_character + " in " + desired_word)
+
+sum: int = 0
+
+if entered_character == desired_word[0]:
+    sum = sum + 1
+    print(entered_character + " found at index 0 ")     
+
+if entered_character == desired_word[1]:
+    sum = sum + 1
+    print(entered_character + " found at index 1 ")
+  
+if entered_character == desired_word[2]:
+    sum = sum + 1
+    print(entered_character + " found at index 2 ")
+      
+if entered_character == desired_word[3]:  
+    sum = sum + 1
+    print(entered_character + " found at index 3 ")
+    
+if entered_character == desired_word[4]:
+    sum = sum + 1
+    print(entered_character + " found at index 4 ")  
+
+if sum > 1:
+    print(sum + " instance of " + entered_character + " found in " + desired_word) 
+
+if sum == 1:
+    print("1 instance of " + entered_character + " found in " + desired_word)
+
+if sum == 0:
+    print("No instance of " + entered_character + " found in " + desired_word )
