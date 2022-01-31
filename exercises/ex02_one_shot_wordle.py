1fde993d181d503e05db202305631c9ee8372f06
Michelle <lemish@ad.unc.edu>
2022-01-31 14:30:54 -0500

    idk

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -2,4 +2,21 @@
 
 __author__= "730407722"
 
+secret_word = str("python") 
+
+guess_word: str = input("What is your 6-letter guess?") 
+
+print("What is your 6-letter guess?")
+
+while len(guess_word) != len(secret_word):
+    guess_word: str = input("That was not 6-letters! Try again: ")
+    
+i: int = 0
+
+while i < len(secret_word):
+    guess_word[i] == secret_word[i]
+
+
+
+
 
