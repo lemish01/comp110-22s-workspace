1bca4223034d4067039e80da68821d4c29cbeffc
Michelle <lemish@ad.unc.edu>
2022-03-24 10:37:33 -0400

    ex07

--- exercises/ex07/data_utils.py
+++ exercises/ex07/data_utils.py
@@ -1,5 +1,90 @@
 """Dictionary related utility functions."""
 
-__author__ = ""
+__author__ = "730407722."
 
-# Define your functions below
+from csv import DictReader
+
+
+def read_csv_rows(filename: str) -> list[dict[str, str]]:
+    """Read the rows fo a csv into a 'table'."""
+    result: list[dict[str, str]] = []
+
+    # Open a handle to the data file 
+    file_handle = open(filename, "r", encoding="utf8")
+   
+    # Prepare to read the data file as a CSV rather than just strings
+    csv_reader = DictReader(file_handle)
+    
+    # Reach each row of the CSV line-by-line
+    for row in csv_reader:
+        result.append(row)
+
+    # Close the file when we're done, to free its resources.
+    file_handle.close()
+    
+    return result 
+
+
+def column_values(table: list[dict[str, str]], column: str) -> list[str]:
+    """Produce a list[str] of all values in a single column."""
+    result: list[str] = []
+    for row in table:
+        item: str = row[column]
+        result.append(item)
+    return result
+
+
+def columnar(row_table: list[dict[str, str]]) -> dict[str, list[str]]:
+    """Transform a row-oriented table to a column-oriented table."""
+    result: dict[str, list[str]] = {}
+    first_row: dict[str, str] = row_table[0]
+    for column in first_row:
+        result[column] = column_values(row_table, column)
+    return result
+
+
+def head(no_mut: dict[str, list[str]], rows_amt: int) -> dict[str, list[str]]:
+    """Produces a new colum based table with only the first N rows of data for each column."""
+    result: dict[str, list[str]] = {}
+    for column in no_mut:
+        M: list[str] = []
+        i: int = 0
+        if len(no_mut[column]) < rows_amt:
+            return no_mut
+        while i < rows_amt:
+            M.append(no_mut[column][i])
+            i += 1
+        result[column] = M
+    return result 
+
+
+def select(og: dict[str, list[str]], copy_over: list[str]) -> dict[str, list[str]]:
+    """Produce a new column-based table with only a specific subset of the original columns."""
+    result: dict[str, list[str]] = {}
+    for column in copy_over:
+        result[column] = og[column]
+    return result 
+
+
+def concat(yo: dict[str, list[str]], oy: dict[str, list[str]]) -> dict[str, list[str]]:
+    """Produce a new column-based table with two column-based tables combined."""
+    result: dict[str, list[str]] = {}
+    for column in yo:
+        result[column] = yo[column]
+    for column in oy:
+        if column in result:
+            result[column] += oy[column]
+        else:
+            result[column] = oy[column]
+    return result
+
+
+def count(key: list[str]) -> dict[str, int]:
+    """Given a list, will return with a dictionary of unique keys and value associated is count of the times key appears."""
+    result: dict[str, int] = {}
+    for item in key:
+        if item in result:
+            result[item] += 1
+        else:
+            result[item] = 1
+    return result 
---
+++ lessons/notebook_helpers.py
@@ -0,0 +1,7 @@
+"""Examples of helpler functions for a notebook."""
+
+ 
+def addTwoInts(x: int, y: int) -> int:
+    """Add x to y."""
+    return x + y
+
